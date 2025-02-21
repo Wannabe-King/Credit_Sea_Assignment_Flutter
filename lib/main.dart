6de98adca30d78c6530d9518eec112b7d183a7e0
Priyanshu Sidar <sidarsunita2@gmail.com>
2025-02-21 15:24:28 +0530

    main changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
-import 'package:creditsea_flutter_assignment/view/screens/applicationstatus.dart';
-import 'package:creditsea_flutter_assignment/view/screens/applyforloan.dart';
-import 'package:creditsea_flutter_assignment/view/screens/email_verification.dart';
-import 'package:creditsea_flutter_assignment/view/screens/loanoffer.dart';
-import 'package:creditsea_flutter_assignment/view/screens/pan_detail.dart';
-import 'package:creditsea_flutter_assignment/view/screens/personal_details.dart';
 import 'package:creditsea_flutter_assignment/view/screens/welcome.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
   await GetStorage.init();
-  await Hive.initFlutter();
-  await Hive.openBox('userBox');
   runApp(const MyApp());
 }
 
